198605f8eabb231fce3e165f6db48f8a383dc1f1
John <zjohnzheng@gmail.com>
2020-07-10 14:19:47 -0700

    Initial Commit

---
+++ ProgressGif/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgressGif
+//
+//  Created by Zheng on 7/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
