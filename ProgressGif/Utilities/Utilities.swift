1cbc43394ea9cea83ad567ce45942cd2b7b6cc50
John <zjohnzheng@gmail.com>
2020-08-03 15:33:12 -0700

    Save clipboard URL to temporary directory

--- ProgressGif/Utilities/Utilities.swift
+++ ProgressGif/Utilities/Utilities.swift
@@ -8,6 +8,18 @@
 import UIKit
 import AVFoundation
 
+extension String {
+    var isValidURL: Bool {
+        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+        if let match = detector.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
+            // it is a link, if the match covers the whole string
+            return match.range.length == self.utf16.count
+        } else {
+            return false
+        }
+    }
+}
+
 class InputViewButton: UIButton {
     
     var viewForInput = UIView()
