2efe38b1a50b98097b276529b1bb29ab168f78ce
John <zjohnzheng@gmail.com>
2020-08-05 16:14:18 -0700

    Add about page + contributing templates

---
+++ ProgressGif/About/Contributors.swift
@@ -0,0 +1,57 @@
+//
+//  Contributors.swift
+//  ProgressGif
+//
+//  Created by Zheng on 8/5/20.
+//
+
+import Foundation
+
+class Contributor: NSObject {
+    
+    /// name to display in the table view
+    var name = "Bob"
+    
+    /// how many lines of code you added
+    var additions = "10"
+    
+    /// how many lines of code you deleted
+    var deletions = "1"
+    
+    var profileName = "You"
+    /// name of the image that your link should display
+    var linkImageName = "photos"
+    
+    /// the link to go to when clicked
+    var link = URL(string: "https://google.com")
+}
+
+extension AboutViewController {
+    
+    // MARK: - Add your name here if you contributed!
+    
+    func populateContributors() {
+        
+        let aheze = Contributor()
+        aheze.name = "Zheng"
+        aheze.additions = "199,405"
+        aheze.deletions = "29,470"
+        aheze.profileName = "ahezeProfile"
+        aheze.linkImageName = "Medium"
+        if let profileURL = URL(string: "https://medium.com/@ahzzheng") {
+            aheze.link = profileURL
+        }
+        contributors.append(aheze)
+        
+        let you = Contributor()
+        you.name = "You could be here!"
+        you.additions = "??"
+        you.deletions = "??"
+        you.profileName = "You"
+        you.linkImageName = "GitHub"
+        if let profileURL = URL(string: "https://github.com/aheze/ProgressGif") {
+            you.link = profileURL
+        }
+        contributors.append(you)
+    }
+}
